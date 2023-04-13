9f7a71b130a9af281d75e46b0c4db6ec9788edb3
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-04-13 19:28:25 -0300

    Fixing App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Footer } from "./components/Footer";
-import { Projects } from "./components/Projects";
 
 function App() {
   return (
