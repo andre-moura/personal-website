1fc44baf04e561f60a1677c14672e46f9205841f
andre-moura <61800450+andre-moura@users.noreply.github.com>
2023-07-08 13:47:21 -0300

    ci: adding pipeline for deploy through gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personal-website/', // Replace with the correct base URL path
   plugins: [react()],
 })
